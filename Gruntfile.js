a480dd6d90788be76d5a5674ade3322b7f492e96
XhmikosR <xhmikosr@users.sourceforge.net>
2013-11-11 12:28:59 +0200

    Add Gruntfile.js.
    Adds JSHint and uglify-js support.

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    uglify: {
+      options: {
+        /*compress: true,*/
+        mangle: true,
+        preserveComments: false,
+        report: 'min'
+      },
+      compress: {
+        src: 'fu.js',
+        dest: 'fu.min.js'
+      }
+    },
+
+    jshint: {
+      files: ['Gruntfile.js', 'fu.js', 'test/*.js'],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    }
+
+  });
+
+  // Load the grunt plugins
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['jshint', 'uglify']);
+};
